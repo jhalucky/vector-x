57c83b46803aec0cb82348c2654b9ff5622a7562
Lucky Jha <jhalucky61@gmail.com>
2025-10-16 21:26:55 +0530

    File added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 
 function App() {
